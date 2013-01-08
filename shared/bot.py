7734397b32427372595e5aa25462c6483a62c0aa
zhk <zhangkangj@gmail.com>
2013-01-08 11:52:09 -0500

    setup

---
+++ shared/bot.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 8, 2013
+
+@author: zhk
+'''
+
+class Bot(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
