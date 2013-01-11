9aa2a62ae7d8cb82e21202cb507a1db0d0be5e9c
zhk <zhangkangj@gmail.com>
2013-01-11 13:41:13 -0500

    remove extra files

--- lib/pbots_calc-master/java/calculator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=/home/zhk/Workspace/Python/pokerbot/lib/pbots_calc-master/export/linux2/lib:$LD_LIBRARY_PATH
-java -cp /home/zhk/Workspace/Python/pokerbot/lib/pbots_calc-master/java/jnaerator-0.11-SNAPSHOT-20121008.jar:/home/zhk/Workspace/Python/pokerbot/lib/pbots_calc-master/java/bin/ pbots_calc/Calculator $@
--- lib/pbots_calc-master/python/calculator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=/home/zhk/Workspace/Python/pokerbot/lib/pbots_calc-master/export/linux2/lib:$LD_LIBRARY_PATH
-python /home/zhk/Workspace/Python/pokerbot/lib/pbots_calc-master/python/calculator.py $@
