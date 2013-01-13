7ec4da7333bdbec1c29f218ae5cab0aaef2b3125
andrsncpr <andrsncpr@andrsncpr-ThinkPad-T400>
2013-01-13 16:12:38 -0500

    preflopBot skeleton

--- shared/preflopBot.py
+++ shared/preflopBot.py
@@ -4,6 +4,12 @@ import argparse
 import socket
 
 class preflopBot(Bot):
+        def initialize(self):
+            super(self)
+            self.oppRange = [None, None]
+            self.oppBBRaiseMatrix
+            self.oppSBRaiseMatrix
+    
         def run(self, input_socket):
             f_in = input_socket.makefile()
             while True:
@@ -14,9 +20,39 @@ class preflopBot(Bot):
     
                 print data
     
-                word = data.split()[0]
+                parts = data.split()
+                word = parts[0]
+                
+                if word == "NEWGAME":
+                    self.oppName = parts[2]
+                    self.stackSize = int(parts[3])
+                    self.bb = int(parts[4])
+                if word == "NEWHAND":
+                    self.button = bool(parts[2])
+                    self.holeCards = [parts[3], parts[4], parts[5]]
+                    self.numBoardCards = 0
                 if word == "GETACTION":
-                    self.socket.send("CHECK\n")
+                    self.potSize = int(parts[1])
+                    numBoardCards = int(parts[1])
+                    
+                    # update the board cards if the street has just changed
+                    if self.numBoardCards != numBoardCards:
+                        self.numBoardCards = numBoardCards
+                        self.boardCards = parts[3:3+numBoardCards]
+                    
+                    if numBoardCards == 0: #preflop
+                        pass
+                    elif numBoardCards == 3: #flop
+                        pass
+                    elif numBoardCards == 4: #turn
+                        pass
+                    else: #river
+                        pass
+                    
+                if word == "HANDOVER": #if opp folds and we haven't checked the board, could read the board here
+                    pass
+                if word == "KEYVALUE":
+                    pass
                 elif word == "REQUESTKEYVALUES":
                     self.socket.send("FINISH\n")
             # Clean up the socket.
