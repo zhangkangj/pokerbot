cff72c0f3293eab055b631742f9805a6982641fd
oo <oo@oo-desktop>
2013-01-17 23:18:04 -0500

    preflop range

--- test/preflopOdd.py
+++ test/preflopOdd.py
@@ -5,14 +5,14 @@ Created on Jan 12, 2013
 '''
 
 from shared.pbots_calc import calc
-from shared.util import number_to_card, card_to_number, draw_cards, hash_cards
+from shared.util import number_to_card, card_to_number, draw_cards, hash_cards, reduce_hand
 from datetime import datetime
 from random import random 
 from shared.calculator import Calculator
 import sys
 
 #myCards must be sorted
-def preflopOdd(myCards, sampleSize = 400):
+def preflopOdd(myCards, opCards = None, sampleSize = 400):
     n = 0
     totalProb = 0
     sampleRate = sampleSize / 18000.0
@@ -27,17 +27,40 @@ def preflopOdd(myCards, sampleSize = 400):
                     continue
                 n += 1
                 board = [i, j, k]
-                totalProb += calculator.flopOdd(myCards, board, None, 400)[2]
+                totalProb += calculator.flopOdd(myCards, board, opCards, 400)[2]
     odd = totalProb / n
     return odd
 
 def computePreFlopOdd(index = 0, start = 0, end = 52):
+    preflopOdds = {}
     out = open("dat/preflopOdd" + str(index) + ".csv", "w")
     for i in range(start, end):
         for j in range(i+1,52):
             for k in range(j+1,52):
                 hashCode = hash_cards([i, j, k])
-                odd = preflopOdd([i,j,k])
+                hashCodeReduced = hash_cards(reduce_hand([i, j, k]))
+                if hashCodeReduced in preflopOdds:
+                    odd = preflopOdds[hashCodeReduced]
+                else:
+                    odd = preflopOdd([i,j,k])
+                    preflopOdd[preflopOdd] = odd
+                out.write(str(hashCode) + "," + str(odd) + "\n")
+                out.flush()
+                
+def computeRangedPreFlopOdd(index = 0, opCards = None):
+    preflopOdds = {}
+    out = open("dat/preflopOdd" + str(index) + ".csv", "w")
+    for i in range(0, 52):
+        for j in range(i+1,52):
+            print index, i, j
+            for k in range(j+1,52):
+                hashCode = hash_cards([i, j, k])
+                hashCodeReduced = hash_cards(reduce_hand([i, j, k]))
+                if hashCodeReduced in preflopOdds:
+                    odd = preflopOdds[hashCodeReduced]
+                else:
+                    odd = preflopOdd([i,j,k], opCards)
+                    preflopOdd[preflopOdd] = odd
                 out.write(str(hashCode) + "," + str(odd) + "\n")
                 out.flush()
                 
@@ -50,4 +73,9 @@ if __name__ == '__main__':
 #        preflopOdd(myCards, 300, None)
 #    print "time:" + str(datetime.now() - start)
 
-    print computePreFlopOdd(8, 26, 52)
+#    index = 0
+#    weights = [0] * 100
+#    for i in range(index*10, index*10+10):
+#        weights[i] = 0.1
+#    opCards = calculator.sampleCards(weights, 400)
+#    print computePreFlopOdd(index, opCards)
