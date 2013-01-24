781de07519298e5f90da85c65aa250faf4a0a920
andrsncpr <andrsncpr@andrsncpr-ThinkPad-T400>
2013-01-23 21:27:27 -0500

    functioning statistician

--- shared/statistician.py
+++ shared/statistician.py
@@ -8,155 +8,198 @@ import math
 
 class Statistician:
     def __init__(self):
+        self.numLevels = 10
         self.initWindowSize = 30
-        self.windowSize = 100
+        self.windowSize = 1000 #must be a multiple of 10
         self.oppRange = [0, 1]
-        self.oppBBFirstTotalTimes = 0
-        self.oppBBFirstRaiseTimes = 0
-        self.oppSBFirstTotalTimes = 0
-        self.oppSBFirstRaiseTimes = 0
-        self.oppBBRaiseMatrix = [[], []]
-        self.oppSBRaiseMatrix = [[], []]
+        
+        #preflop
+        self.SBNumFirstRounds = 0 #the total number of first rounds played so far from SB position 
+        self.SBNumFirstRaises = 0 #the total number of first raises by opp from SB position
+        self.SBRaiseMatrix = [[], []] #opp's raise amounts from the previous self.windowSize rounds (organized by round type)
+        self.BBNumFirstRounds = 0
+        self.BBNumFirstRaises = 0
+        self.BBRaiseMatrix = [[], []]
     
     def processHandHist(self, oppName, button, recentActions):
-        pass
-#        street = 0
-#        raiseRound = 0
-#        
-#        for action in recentActions:
-#            if action[-1] == oppName:
-#                if street == 0 and raiseRound < 2:
-#                    self.processOppRaiseStats(button, raiseRound, action)
-#                else:
-#                    pass
-#                raiseRound += 1
-#            elif action[0] == "DEAL":
-#                street += 1
-#                raiseRound = 0
-    
-    def getOppRange(self):
-        oppRange = range(10)
-        lowerBound = 10 - math.ceil(self.oppRange[1]*10)
-        upperBound = 10 - math.floor(self.oppRange[0]*10)
-        for i in oppRange:
-            if i >= lowerBound and i < upperBound:
-                oppRange[i] = 1
-            else:
-                oppRange[i] = 0
-        return oppRange
-    
-    def processOppRaiseStats(self, dealer, raiseRound, oppAction):
-        raiseRatio = None
+        street = 0
+        raiseRound = 0
+        
+        for action in recentActions:
+            if action[-1] == oppName:
+                if street == 0 and raiseRound < 2:
+                    self.processOppRaiseStats(button, raiseRound, action)
+                else: #stats for other streets and preflop raises after two rounds
+                    pass
+                raiseRound += 1
+            elif action[0] == "DEAL":
+                street += 1
+                raiseRound = 0
+
+    def getPreflopDist(self, dealer, raiseRound, oppAction):
+        oppRange = self.getPreflopRange(dealer, raiseRound, oppAction)
+        oppLevels = self.fromRangeToLevels(oppRange)
+        return self.fromLevelsToDist(oppLevels, numLevels = self.numLevels)
+
+    def getPreflopRange(self, dealer, raiseRound, oppAction):
+        raiseAmount = self.getRaiseAmount(oppAction)
         
         if dealer:
-            if raiseRound == 0:
-                self.oppBBFirstTotalTimes += 1
-            
-            if oppAction[0] == "FOLD":                                   
-                raiseRatio = -1
-            elif oppAction[0] == "CALL":
-                raiseRatio = 0
-            elif oppAction[0] == "RAISE" and raiseRound == 0:
-                raiseRatio = oppAction[1]             
-                self.oppBBFirstRaiseTimes += 1
-            
-            self.oppBBRaiseMatrix[raiseRound].append(raiseRatio)
-            if len(self.oppBBRaiseMatrix[raiseRound]) > self.windowSize:
-                self.oppBBRaiseMatrix[raiseRound].pop(0)            
-            
-            # if we don't have enough cases
-            if len(self.oppBBRaiseMatrix[raiseRound]) < self.initWindowSize:
-                if raiseRound == 0:
-                    if oppAction[0] == "FOLD":
-                        self.oppRange = [0.5,1]
-                    elif oppAction[0] == "CALL":
-                        self.oppRange = [0.2,0.5]
-                    elif oppAction[0] == "RAISE":
-                        self.oppRange = [0,0.2]
-                        
-                if raiseRound == 1:
-                    if oppAction[0] =="FOLD":
-                        self.oppRange = [0.1,0.2]
-                    elif oppAction[0] == "CALL":
-                        self.oppRange = [0.05,0.1]
-                    elif oppAction[0] == "RAISE":
-                        self.oppRange = [0,0.05]
-            else:
-                numLarger = 0
-                numSmaller = 0
-            
-                for num in self.oppBBRaiseMatrix[raiseRound]:
-                    if num > raiseRatio:
-                        numLarger += 1
-                    elif num < raiseRatio:
-                        numSmaller += 1
-                        
-                if raiseRound == 0:
-                    lowPercent = int(100 * float(numLarger) / len(self.oppBBRaiseMatrix[raiseRound]))
-                    highPercent = int(100 * (1 - float(numSmaller) / len(self.oppBBRaiseMatrix[raiseRound])))
-                elif raiseRound == 1:
-                    print self.oppBBFirstTotalTimes
-                    print len(self.oppBBRaiseMatrix[raiseRound])
-                    lowPercent = int(100 * (float(self.oppBBFirstRaiseTimes) / self.oppBBFirstTotalTimes) * (float(numLarger) / len(self.oppBBRaiseMatrix[raiseRound])))
-                    highPercent = int(100 * (float(self.oppBBFirstRaiseTimes) / self.oppBBFirstTotalTimes) * (1 - float(numSmaller) / len(self.oppBBRaiseMatrix[raiseRound])))
-              
-                if highPercent - lowPercent < 10:
-                    highPercent = min(100, highPercent + (1 + 10 - (highPercent - lowPercent)) / 2)
-                    lowPercent = max(0, lowPercent - (10 - (highPercent - lowPercent)) / 2)
-                    
-                self.oppRange = [float(lowPercent) / 100, float(highPercent) / 100]
-        else:  
-            if raiseRound == 0:
-                self.oppSBFirstTotalTimes += 1
-            
-            if oppAction[0] == "FOLD":                                   
-                raiseRatio = -1
-            elif oppAction[0] == "CALL":
-                raiseRatio = 0
-            elif oppAction[0] == "RAISE" and raiseRound == 0:
-                raiseRatio = oppAction[1]           
-                self.oppSBFirstRaiseTimes += 1
+            return self.getSBPreflopRange(raiseRound, raiseAmount)
+        else:
+            return self.getBBPreflopRange(raiseRound, raiseAmount)
+
+    # recording sub-methods
+    def processOppRaiseStats(self, dealer, raiseRound, oppAction):
+        if dealer:
+            self.updateSBPreflopStats(raiseRound, oppAction)
+        else:
+            self.updateBBPreflopStats(raiseRound, oppAction)
+
+    def updateSBPreflopStats(self, raiseRound, oppAction):
+        if raiseRound == 0:
+            self.SBNumFirstRounds += 1
+            if oppAction[0] == "RAISE":
+                self.SBNumFirstRaises += 1
                 
-            self.oppSBRaiseMatrix[raiseRound].append(raiseRatio)
-            if len(self.oppSBRaiseMatrix[raiseRound]) > self.windowSize:
-                self.oppSBRaiseMatrix[raiseRound].pop(0)                                
+        raiseAmount = self.getRaiseAmount(oppAction)
+        self.SBRaiseMatrix[raiseRound].append(raiseAmount)
+        # if the size of the list is too big
+        if len(self.SBRaiseMatrix[raiseRound]) > self.windowSize:
+            self.SBRaiseMatrix[raiseRound].pop(0)
                 
-            if len(self.oppSBRaiseMatrix[raiseRound]) < self.initWindowSize:
-                if raiseRound == 0:
-                    if oppAction[0] == "FOLD":
-                        self.oppRange = [0.9,1]
-                    elif oppAction[0] == "CALL":
-                        self.oppRange = [0.6,0.9]
-                    elif oppAction[0] == "RAISE":
-                        self.oppRange = [0,0.6]
-                        
-                if raiseRound == 1:
-                    if oppAction[0] =="FOLD":
-                        self.oppRange = [0.5,0.6]
-                    elif oppAction[0] == "CALL":
-                        self.oppRange = [0.1,0.5]
-                    elif oppAction[0] == "RAISE":
-                        self.oppRange = [0,0.1]
-            else:
-                numLarger = 0
-                numSmaller = 0
-            
-                for num in self.oppSBRaiseMatrix[raiseRound]:
-                    if num > raiseRatio:
-                        numLarger += 1
-                    elif num < raiseRatio:
-                        numSmaller += 1
-                        
-                if raiseRound == 0:
-                    lowPercent = int(100 * float(numLarger) / len(self.oppSBRaiseMatrix[raiseRound]))
-                    highPercent = int(100 * (1 - float(numSmaller) / len(self.oppSBRaiseMatrix[raiseRound])))
-                elif raiseRound == 1:
-                    lowPercent = int(100 * (float(self.oppSBFirstRaiseTimes) / self.oppSBFirstTotalTimes) * (float(numLarger) / len(self.oppSBRaiseMatrix[raiseRound])))
-                    highPercent = int(100 * (float(self.oppSBFirstRaiseTimes) / self.oppSBFirstTotalTimes) * (1 - float(numSmaller) / len(self.oppSBRaiseMatrix[raiseRound])))
-              
-                if highPercent - lowPercent < 10:
-                    highPercent = min(100, highPercent + (1 + 10 - (highPercent - lowPercent)) / 2)
-                    lowPercent = max(0, lowPercent - (10 - (highPercent - lowPercent)) / 2)
+    def updateBBPreflopStats(self, raiseRound, oppAction):
+        if raiseRound == 0:
+            self.BBNumFirstRounds += 1
+            if oppAction[0] == "RAISE":
+                self.BBNumFirstRaises += 1
+                
+        raiseAmount = self.getRaiseAmount(oppAction)
+        self.BBRaiseMatrix[raiseRound].append(raiseAmount)
+        # if the size of the list is too big
+        if len(self.BBRaiseMatrix[raiseRound]) > self.windowSize:
+            self.BBRaiseMatrix[raiseRound].pop(0)    
+
+    # ranging sub-methods    
+    def getSBPreflopRange(self, raiseRound, raiseAmount):
+        # if we don't have enough cases
+        if len(self.SBRaiseMatrix[raiseRound]) < self.initWindowSize:
+            if raiseRound == 0:
+                if raiseAmount == -1:
+                    return [0.5,1]
+                elif raiseAmount == 0:
+                    return [0.2,0.5]
+                else:
+                    return [0,0.2]
                     
-                self.oppRange = [float(lowPercent) / 100, float(highPercent) / 100]
-                            
+            if raiseRound == 1:
+                if raiseAmount == -1:
+                    return [0.1,0.2]
+                elif raiseAmount == 0:
+                    return [0.05,0.1]
+                else:
+                    return [0,0.05]
+        else:
+            oppRange = self.compareNumToArray(raiseAmount, self.SBRaiseMatrix[raiseRound])
+                    
+            if raiseRound == 1:
+                raisePercentage = float(self.SBNumFirstRaises) / self.SBNumFirstRounds
+                self.refineOppRange(raisePercentage, oppRange)
+                
+            return oppRange
+                    
+    def getBBPreflopRange(self, raiseRound, raiseAmount):
+        if len(self.BBRaiseMatrix[raiseRound]) < self.initWindowSize:
+            if raiseRound == 0:
+                if raiseAmount == -1:
+                    return [0.9,1]
+                elif raiseAmount == 0:
+                    return [0.6,0.9]
+                else:
+                    return [0,0.6]
+                    
+            if raiseRound == 1:
+                if raiseAmount == -1:
+                    return [0.5,0.6]
+                elif raiseAmount == 0:
+                    return [0.1,0.5]
+                else:
+                    return [0,0.1]
+        else:
+            oppRange = self.compareNumToArray(raiseAmount, self.BBRaiseMatrix[raiseRound])
+                    
+            if raiseRound == 1:
+                raisePercentage = float(self.BBNumFirstRaises) / self.BBNumFirstRounds
+                self.refineOppRange(raisePercentage, oppRange)
+                
+            return oppRange    
+        
+    # other helper sub-methods    
+    # action is an array whose first element is its type, e.g. ['RAISE', 7, 'P1']
+    def getRaiseAmount(self, action):
+        if action[0] == "FOLD":
+            return -1
+        elif action[0] == "CALL":
+            return 0
+        elif action[0] == "RAISE":
+            return action[1]
+    
+    def refineOppRange(self, percentage, oppRange):
+        [a, b] = oppRange
+        delta = (b - a) * percentage
+        
+        return [a, a + delta]
+    
+    def compareNumToArray(self, num, array):
+        arrayCopy = list(array)
+        arrayCopy.sort()
+        arrayCopy.reverse()
+        
+        numBigger = None
+        numAppearances = arrayCopy.count(num)
+        
+        if numAppearances == 0:
+            for i in range(len(arrayCopy)):
+                if arrayCopy[i] < num:
+                    numBigger = i
+                    break
+        else:
+            numBigger = arrayCopy.index(num)
+                
+        return [float(numBigger)/len(array), float(numBigger+numAppearances)/len(array)]
+    
+    def fromRangeToLevels(self, oppRange, numLevels = 10):
+        [a, b] = oppRange
+        a = int(math.floor(numLevels*a))
+        b = int(math.ceil(numLevels*b))
+        
+        if a == b:
+            return [a, b+1]
+        else:
+            return [a, b]
+    
+    def fromLevelsToDist(self, levels, numLevels = 10):
+        lowerBound = numLevels - levels[1]
+        upperBound = numLevels - levels[0]
+        
+        levelArray = range(numLevels)                
+        for i in levelArray:
+            if i >= lowerBound and i < upperBound:
+                levelArray[i] = 1
+            else:
+                levelArray[i] = 0
+        return levelArray                          
+                                 
+if __name__ == "__main__":
+    stat = Statistician()
+
+#    levels = [[0, 10], [0, 5], [5, 10], [2, 7]]
+#    for level in levels:
+#        print stat.fromLevelsToDist(level)
+        
+#    arrays = [[12, 32, 90, 9, 3, 23, 100, 43, 98, 100],
+#              [12, 33, 33, 33,3, 23, 100, 43, 98, 100],
+#              [12, 33, 90, 9, 3, 23, 100, 43, 98, 100]]
+#    for array in arrays:
+#        oppRange = stat.compareNumToArray(33, array)
+#        print stat.fromRangeToLevels(oppRange, numLevels = 5)
